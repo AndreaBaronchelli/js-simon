7abfdddc03e05d965cfddc261098c8dd99830405
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-12 14:15:49 +0200

    finished script

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 var numbers = [];
 var userNumbers = [];
+var checkedNumbers = [];
 var quantità = 5;
 
 // Creare arrey di 5 numeri casuali
@@ -13,21 +14,40 @@ while(numbers.length < quantità) {
         numbers.push(number);
     }  
 };
-console.log(numbers);
+//console.log(numbers);
 
 // Output numeri da memorizzare
 alert("I numeri da memorizzare sono: " + numbers);
 
-// Chiedere di inserire i numeri memorizzati
+// Chiedere di inserire i numeri memorizzati dopo 30s
 setTimeout(function(){
     while(userNumbers.length < quantità) {
         var userNumber = parseInt( prompt("Inserisci il numero " + (userNumbers.length + 1) + " di " + quantità) );
+        
+        //Validazione
+        while(userNumber < 0 || userNumber > 100 || isNaN(userNumber)) {
+            var userNumber = parseInt( prompt("Valore non valido.\nInserisci il numero " + (userNumbers.length + 1) + " di " + quantità) );
+        }
+        while(userNumbers.includes(userNumber)) {
+            var userNumber = parseInt( prompt("Numero già inserito.\nInserisci il numero " + (userNumbers.length + 1) + " di " + quantità) )
+        }
+
         userNumbers.push(userNumber);
     }
-    console.log(userNumbers);
+    //console.log(userNumbers);
+
+    //Controllo numeri indovinati
+    for (var i = 0; i < userNumbers.length; i++) {
+        if( numbers.includes(userNumbers[i]) ) {
+            checkedNumbers.push(userNumbers[i]);
+        }
+        
+    }
+
+    //Output
+    console.log("Hai indovinato " + checkedNumbers.length + " numeri su " + numbers.length);
+    console.log("I numeri indovinati sono: " + checkedNumbers);
 }, 30000)
-
-
 
 
 
